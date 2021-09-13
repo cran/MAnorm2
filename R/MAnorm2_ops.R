466735e925f87995f2ccae133a3eaa1dcb84561b
Shiqi Tu <tushiqi@picb.ac.cn>
2021-09-13 09:30:05 +0000

    version 1.2.0

---
+++ R/MAnorm2_ops.R
@@ -0,0 +1,11 @@
+# For loading operations and startup message.
+#
+# Last update: 2021-09-10
+
+
+# Display startup message
+.onAttach <- function(libname, pkgname) {
+    packageStartupMessage("MAnorm2 1.2.0 2021-09-10")
+}
+
+
